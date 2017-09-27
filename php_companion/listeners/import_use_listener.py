f7de900837864984808e31e3ce4add4eefece3a5
qkdreyer <quentin.dreyer@triotech.fr>
2017-09-27 16:59:36 +0200

    Removing unused uses

--- php_companion/listeners/import_use_listener.py
+++ php_companion/listeners/import_use_listener.py
@@ -8,7 +8,9 @@ from ..utils import find_symbol
 
 class ImportUseListener(sublime_plugin.EventListener):
     def index_symbols_by_category(self, carry, item):
-        pos = item[0].begin()
+        pos = item[0]
+        begin = pos.begin()
+
         symbol = item[1]
         chunks = symbol.split(':')
         category = chunks[0].lstrip()
@@ -17,9 +19,9 @@ class ImportUseListener(sublime_plugin.EventListener):
         if not carry.get(category):
             carry[category] = {}
 
-        if not carry[category].get(klass) and self.view.substr(sublime.Region(pos - 1, pos)) != '\\':
+        if not carry[category].get(klass) and self.view.substr(sublime.Region(begin - 1, begin)) != '\\':
             key = klass.rsplit('\\', 1)[-1]
-            carry[category][key] = klass
+            carry[category][key] = [klass, pos]
 
         return carry
 
@@ -42,18 +44,26 @@ class ImportUseListener(sublime_plugin.EventListener):
 
         if enable_import_use_on_save and file_name.endswith('.php'):
             symbols = reduce(self.index_symbols_by_category, view.symbols(), {})
-            self.namespace = list(symbols.get('N').items())[0][1]
+            self.namespace = list(symbols.get('N').items())[0][1][0]
             uses = self.merge_symbols_for_categories(symbols, ['SU', 'SUA'])
             klasses = self.merge_symbols_for_categories(symbols, ['SC', 'SCA', 'SE'])
+            print('symbols', symbols)
 
             for klass in klasses:
                 if not uses.get(klass):
+                    print('adding klass', klass, klasses[klass])
                     self.namespaces = find_symbol(klass, view.window())
 
                     if len(self.namespaces) == 1:
                         self.on_done(0)
                     elif len(self.namespaces) > 1:
                         view.window().show_quick_panel(self.namespaces, self.on_done)
+
+            for use in uses:
+                if not klasses.get(use):
+                    print('removing use', use, uses[use])
+                    region = uses[use][1]
+                    self.view.run_command("replace_fqcn", {"region_start": region.begin() - 5, "region_end": region.end() + 1, "namespace": ''})
 
     def on_done(self, index):
         if index == -1:
