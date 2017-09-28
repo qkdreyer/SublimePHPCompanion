253037dcfd44df6debc8dea465db71d45cf7d550
qkdreyer <quentin.dreyer@triotech.fr>
2017-09-28 14:17:50 +0200

    Saving view once uses have been added/removed

--- php_companion/listeners/import_use_listener.py
+++ php_companion/listeners/import_use_listener.py
@@ -1,12 +1,69 @@
 import sublime
 import sublime_plugin
 import re
+import sys
 from functools import reduce
 
 from ..settings import get_setting
 from ..utils import find_symbol
 
 class ImportUseListener(sublime_plugin.EventListener):
+    def on_post_save_async(self, view):
+        self.view = view
+        settings = sublime.load_settings('PHP Companion.sublime-settings')
+        enable_import_use_on_save = settings.get('enable_import_use_on_save', False)
+        file_name = view.file_name()
+
+        if isinstance(enable_import_use_on_save, str):
+            search = re.search(enable_import_use_on_save, file_name)
+            enable_import_use_on_save = search != None
+
+        if enable_import_use_on_save and file_name.endswith('.php'):
+            symbols = reduce(self.index_symbols_by_category, view.symbols(), {})
+            self.imports = self.merge_symbols_for_categories(symbols, ['SU', 'SUA'])
+            self.references = self.merge_symbols_for_categories(symbols, ['SP', 'SC', 'SCA'])
+            #print('symbols', symbols)
+
+            self.namespace = self.nextval(symbols.get('N'))
+            self.klass = self.nextval(symbols.get('C'))
+
+            for klass, use in self.imports.items():
+                if not self.references.get(klass):
+                    #print('removing use', klass, use)
+                    region = self.view.find(('use ' + use.get('fqcn') + ';').replace('\\', '\\\\'), 0)
+                    self.view.run_command('replace_fqcn', {'region_start': region.begin() -1, 'region_end': region.end(), 'namespace': '', 'leading_separator': False})
+
+            self.references_iterator = iter(self.references.values())
+            self.on_select()
+
+    def on_select(self):
+        try:
+            reference = next(self.references_iterator)
+            klass = reference.get('klass')
+            if not self.imports.get(klass):
+                self.namespaces = find_symbol(klass, self.view.window())
+
+                if len(self.namespaces) == 1:
+                    self.on_import(0)
+                elif len(self.namespaces) > 1:
+                    self.view.window().show_quick_panel(self.namespaces, self.on_import)
+            else:
+                self.on_select()
+        except StopIteration:
+            if self.view.is_dirty():
+                self.view.run_command('save')
+
+    def on_import(self, index):
+        if index == -1:
+            return
+
+        namespace = self.namespaces[index][0]
+        if not self.namespace.get('fqcn') in namespace:
+            #print('adding use', namespace)
+            self.view.run_command('import_use', {'namespace': namespace})
+
+        self.on_select()
+
     def index_symbols_by_category(self, carry, item):
         pos = item[0]
         begin = pos.begin()
@@ -38,61 +95,3 @@ class ImportUseListener(sublime_plugin.EventListener):
 
     def nextval(self, dict):
         return next(iter(dict.values()))
-
-    def on_pre_save(self, view):
-        self.view = view
-        settings = sublime.load_settings('PHP Companion.sublime-settings')
-        enable_import_use_on_save = settings.get('enable_import_use_on_save', False)
-        file_name = view.file_name()
-
-        if isinstance(enable_import_use_on_save, str):
-            search = re.search(enable_import_use_on_save, file_name)
-            enable_import_use_on_save = search != None
-
-        if enable_import_use_on_save and file_name.endswith('.php'):
-            symbols = reduce(self.index_symbols_by_category, view.symbols(), {})
-            self.imports = self.merge_symbols_for_categories(symbols, ['SU', 'SUA'])
-            self.references = self.merge_symbols_for_categories(symbols, ['SP', 'SC', 'SCA'])
-            #print('symbols', symbols)
-
-            self.namespace = self.nextval(symbols.get('N'))
-            self.klass = self.nextval(symbols.get('C'))
-
-            for klass, use in self.imports.items():
-                if not self.references.get(klass): #and use.get('pos').begin() < self.klass.get('pos').begin():
-                    #print('removing use', klass, use)
-                    region = self.view.find(('use ' + use.get('fqcn') + ';').replace('\\', '\\\\'), 0)
-                    self.view.run_command('replace_fqcn', {'region_start': region.begin() -1, 'region_end': region.end(), 'namespace': '', 'leading_separator': False})
-
-            self.references_iterator = iter(self.references.values())
-            self.on_select()
-
-    def on_select(self):
-        try:
-            reference = next(self.references_iterator)
-            klass = reference.get('klass')
-            if not self.imports.get(klass):
-                self.namespaces = find_symbol(klass, self.view.window())
-                #print('adding?', self.namespaces)
-
-                if len(self.namespaces) == 1:
-                    self.on_import(0)
-                elif len(self.namespaces) > 1:
-                    self.view.window().show_quick_panel(self.namespaces, self.on_import)
-            else:
-                self.on_select()
-        except StopIteration:
-            self.view.run_command('save')
-
-    def on_import(self, index):
-        if index == -1:
-            return
-
-        namespace = self.namespaces[index][0]
-        #print('namespaces', namespace, self.namespace.get('fqcn'), index)
-        if not self.namespace.get('fqcn') in namespace:
-            #print('adding use', namespace)
-            self.view.run_command('import_use', {'namespace': namespace})
-
-        self.on_select()
-
